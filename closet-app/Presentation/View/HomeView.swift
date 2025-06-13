885fd3df3f74e31836e66ab3ecdcdbff1a411c5c
shorei <160503699+gao0325ts@users.noreply.github.com>
2025-06-13 11:18:29 +0900

    デバッグ用のアイテム表示を削除

--- closet-app/Presentation/View/HomeView.swift
+++ closet-app/Presentation/View/HomeView.swift
@@ -22,7 +22,6 @@ struct HomeView: View {
                         .padding(.top, 50)
                     CloseSelectView()
                         .padding(.top, 20)
-                    ClosetItemListView()
                 }
 
                 Button(action: {
