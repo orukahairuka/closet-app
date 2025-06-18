b22d45353eef9c77ad26f30e785693c98706f993
rio <g2554303@cc.kyoto-su.ac.jp>
2025-06-18 19:25:58 +0900

    loading UI作りました

---
+++ closet-app/Presentation/View/loadingView.swift
@@ -0,0 +1,18 @@
+//
+//  loadingView.swift
+//  closet-app
+//
+//  Created by 前田 梨緒 on 2025/06/15.
+//
+
+import SwiftUI
+
+struct loadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loadingView()
+}
